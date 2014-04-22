445affb372493ca2c195c101023b35f13e16be8a
Fabio Ngo <ngolebaotrung@gmail.com>
2014-04-23 03:11:06 +0700

    update Game UI

--- CorrectIsWrong/Main/src/main/java/fabio/correctiswrong/main/Game.java
+++ CorrectIsWrong/Main/src/main/java/fabio/correctiswrong/main/Game.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Handler;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -40,15 +42,21 @@ public class Game extends Activity {
     int current;
     boolean left;
     boolean inGame;
-    Button buttonLeft, buttonRight,startGame;
+    Button buttonLeft, buttonRight;
+    ImageButton startGame;
     int time;
     int TimeLimit;
     Runnable runnable;
     Handler handler;
     boolean win;
-    Chronometer chronometer;
+    TextView timerText;
+    TextView question;
+    TextView gameOver;
+    int screenHeight;
+    int screenWidth;
 
     public Game () {
+
         TimeLimit = 5;
         time = TimeLimit;
         inGame = false;
@@ -57,21 +65,30 @@ public class Game extends Activity {
         score = 0;
         current = 0;
         win = false;
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        //read highScore
-//        chronometer = (Chronometer)findViewById(R.id.chronometer);
-//        chronometer.setBase(SystemClock.elapsedRealtime());
-         handler = new Handler();
+        setContentView(R.layout.game);
+        /**
+         *Get Screen Attributes
+         *
+         *
+         */
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenHeight = displayMetrics.heightPixels;
+        screenWidth = displayMetrics.widthPixels;
+        /**
+         * Timer
+         */
+        timerText = (TextView)findViewById(R.id.time);
+        timerText.setHeight(screenHeight/4);
+        handler = new Handler();
         runnable = new Runnable() {
-
             public void run() {
-
-
                     doStuff();
                 if(time >0) {
                     handler.postDelayed(this, 1000);
@@ -81,24 +98,18 @@ public class Game extends Activity {
                     endGame();
                 }
 
-  /*
-   * Now register it for running next time
-   */
-
-
             }
-
-
         };
+        /**
+         * Import Highscore from file
+         */
         File file = new File(getExternalFilesDir(ACCESSIBILITY_SERVICE),"score.ciw");
-        //System.out.println(file.getAbsolutePath());
         try {
             FileInputStream fileInputStream = new FileInputStream(file);
             try {
                 int temp;
                 do {
                     temp = fileInputStream.read();
-                    //System.out.println(temp);
                     if (temp != -1) {
                         highScore = highScore * 255 + temp;
                     }
@@ -113,14 +124,19 @@ public class Game extends Activity {
                 FileOutputStream fileOutputStream = new FileOutputStream(file);
                 fileOutputStream.write(0);
                 highScore = 0;
+
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
 
         }
-        //Import questionairs
+        /**
+         *
+         *
+         * Import questionairs
+         */
         questions = new Vector<Question>();
-        InputStream inputStream = null;
+        InputStream inputStream;
         try {
             inputStream = getAssets().open("QuestionBank.ciw");
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
@@ -134,21 +150,55 @@ public class Game extends Activity {
         } catch (IOException e) {
             Log.e("tag", "Fail Open QuestionBank");
         }
-        setContentView(R.layout.game);
+        /**
+         * GameOver
+         *
+         */
+        gameOver = (TextView)findViewById(R.id.gameOver);
+        gameOver.setHeight(3*screenHeight/4);
+        gameOver.setVisibility(View.INVISIBLE);
+        /**
+         * Question Text
+         */
+        question = (TextView)findViewById(R.id.QuestionContent);
+        question.setHeight(screenHeight/4);
+        /**
+         * Score Text
+         */
         TextView scoreText = (TextView)findViewById(R.id.score);
-        scoreText.setText(score+"");
-        startGame = (Button)findViewById(R.id.start);
-        buttonLeft = (Button)findViewById(R.id.answer1);
+        scoreText.setHeight(screenHeight/8);
+        scoreText.setWidth(screenWidth/3);
+        TextView newScore = (TextView)findViewById(R.id.newScore);
+        newScore.setHeight(screenHeight/8);
+        newScore.setWidth(screenWidth/3);
+        /**
+         * Score Text
+         */
+        TextView bestScoreText = (TextView)findViewById(R.id.best);
+        bestScoreText.setHeight(screenHeight/8);
+        bestScoreText.setWidth(screenWidth/3);
+        bestScoreText.setText(String.valueOf(highScore));
+        TextView bestScore = (TextView)findViewById(R.id.bestScore);
+        bestScore.setHeight(screenHeight/8);
+        bestScore.setWidth(screenWidth/3);
+        /**
+         * Start Button
+         */
+        startGame = (ImageButton)findViewById(R.id.start);
+        /**
+         * Button Answer Left
+         */
+        buttonLeft = (Button)findViewById(R.id.answerLeft);
+        buttonLeft.setWidth(screenWidth/2);
+        buttonLeft.setHeight(screenHeight/4);
         buttonLeft.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
                 if(inGame){
                     if(!left){
-                        buttonLeft.setBackgroundColor(Color.GREEN);
                         nextQuestion();
 
                     }else{
-                        buttonLeft.setBackgroundColor(Color.RED);
                         win = false;
                         endGame();
                     }
@@ -157,16 +207,19 @@ public class Game extends Activity {
 
             }
         });
-        buttonRight = (Button)findViewById(R.id.answer2);
+        /**
+         * Button Answer Right
+         */
+        buttonRight = (Button)findViewById(R.id.answerRight);
+        buttonRight.setWidth(screenWidth/2);
+        buttonRight.setHeight(screenHeight/4);
         buttonRight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
                 if(inGame) {
                     if(left){
-                        buttonRight.setBackgroundColor(Color.GREEN);
                         nextQuestion();
                     }else{
-                        buttonRight.setBackgroundColor(Color.RED);
                         win =false;
                         endGame();
                     }
@@ -187,23 +240,18 @@ public class Game extends Activity {
 
     private void doStuff () {
         time--;
-        startGame.setText(String.valueOf(time)+"s");
-
-
-
+        timerText.setText(String.valueOf(time));
     }
 
     private void startGame () {
-//        chronometer.start();
         inGame = true;
+        gameOver.setVisibility(View.INVISIBLE);
         win = false;
         time = TimeLimit;
         score = 0;
         TextView scoreText = (TextView)findViewById(R.id.score);
         scoreText.setText(String.valueOf(score));
         current = 0;
-        buttonLeft.setBackgroundColor(Color.BLACK);
-        buttonRight.setBackgroundColor(Color.BLACK);
         runnable.run();
         Collections.shuffle(questions);
         Question cur = questions.get(current);
@@ -224,15 +272,16 @@ public class Game extends Activity {
     private void endGame () {
         inGame = false;
         handler.removeCallbacks(runnable);
+        gameOver.setVisibility(View.VISIBLE);
         if(win){
-            Button b = (Button)findViewById(R.id.start);
-            b.setText("You Win. Tap to start a new game");
+
         }else{
-            Button b = (Button)findViewById(R.id.start);
-            b.setText("You lose. Tap to retry");
+
         }
         if(highScore<score){
             highScore = score;
+            TextView highScoreText = (TextView)findViewById(R.id.best);
+            highScoreText.setText(String.valueOf(highScore));
             File file = new File(getExternalFilesDir(ACCESSIBILITY_SERVICE),"score.ciw");
             FileOutputStream fileOutputStream = null;
             try {
@@ -261,8 +310,6 @@ public class Game extends Activity {
             endGame();
             return;
         }
-        buttonLeft.setBackgroundColor(Color.BLACK);
-        buttonRight.setBackgroundColor(Color.BLACK);
         TextView question = (TextView)findViewById(R.id.QuestionContent);
         question.setText(cur.content);
         Random random = new Random();
