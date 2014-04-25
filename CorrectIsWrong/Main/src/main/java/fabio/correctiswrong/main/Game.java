3a7085b1a64678f8dd961a21ac5460d344b117fc
Fabio Ngo <ngolebaotrung@gmail.com>
2014-04-25 16:45:56 +0700

    Fix Game UI

--- CorrectIsWrong/Main/src/main/java/fabio/correctiswrong/main/Game.java
+++ CorrectIsWrong/Main/src/main/java/fabio/correctiswrong/main/Game.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,6 +16,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -38,7 +43,7 @@ public class Game extends Activity {
     boolean left;
     boolean inGame;
     Button buttonLeft, buttonRight;
-    ImageButton startGame;
+    ImageView startGame;
     int time;
     int TimeLimit;
     Runnable runnable;
@@ -199,7 +204,23 @@ public class Game extends Activity {
         /**
          * Start Button
          */
-        startGame = (ImageButton)findViewById(R.id.start);
+        startGame = (ImageView)findViewById(R.id.start);
+        Drawable drawable = startGame.getDrawable();
+        Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
+        int size = (int)(screenWidth*(0.617-0.417));
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap,size,size,true);
+
+        startGame.setImageBitmap(scaledBitmap);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(size,size);
+        params.addRule(RelativeLayout.CENTER_IN_PARENT);
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                if(!inGame) startGame();
+            }
+        });
+
+
         /**
          * Share button
          */
@@ -261,14 +282,6 @@ public class Game extends Activity {
                 }
 
 
-            }
-        });
-        startGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v) {
-                if(!inGame) {
-                    startGame();
-                }
             }
         });
     }
